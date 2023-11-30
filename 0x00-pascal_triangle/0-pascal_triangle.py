ec17f2aabc5f382dad692a6e7239f9c86f84a144
Paul Njuga <paulnjuga@gmail.com>
2023-12-01 02:25:44 +0300

    Add pascal's triangle implementaion

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to the nth row.
+
+    Args:
+        n (int): The number of rows for Pascal's Triangle.
+
+    Returns:
+        list of lists: A list of lists representing Pascal's Triangle up to the nth row.
+
+    Note:
+        Returns an empty list if n <= 0. Assumes n will always be an integer.
+    """
+    # Check if n is less than or equal to 0
+    if n <= 0:
+        return []
+
+    # Initialize Pascal's Triangle with the first row
+    triangle = [[1]]
+
+    # Generate subsequent rows
+    for i in range(1, n):
+        row = [1]
+        # Compute values for the current row
+        for j in range(1, i):
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        row.append(1)
+        # Append the current row to the triangle
+        triangle.append(row)
+
+    return triangle
+
+# Test the function
+if __name__ == "__main__":
+    n = 5
+    result = pascal_triangle(n)
+    # Print each row of Pascal's Triangle
+    for row in result:
+        print(row)
+
