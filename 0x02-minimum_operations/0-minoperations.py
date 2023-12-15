5ef5be9d8e2ba2a514063c49efb3c548fdf9867e
Paul Njuga <paulnjuga@gmail.com>
2023-12-15 04:17:17 +0300

    Add min ops

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Minimum Operations
+"""
+def minOperations(n):
+    """
+    In a text file, there's a single character H.
+    The text editor can execute only two operations,
+    in the file: Copy All and Paste. Given a number n,
+    Calculate the fewest no. of ops that result in,
+    exactly n H characters in the file
+    """
+    if n <= 1:
+        return 0
+    for op in range(2, n + 1):
+        if n % op == 0:
+            return minOperations(int(n / op)) + op
